0caada67500f8d10b255004e5141aad875807dbe
Naresh Verma <learnnaresh7566@gmail.com>
2024-03-22 18:55:24 +0530

    Add Access token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
 //@desc Register a user
@@ -41,9 +42,31 @@ const registerUser = asyncHandler (async (req , res) =>{
 //@route POST /api/users/login
 //@access public
 
-const loginUser = asyncHandler (async (req , res) =>{
-    const user = await User.find()
-    res.status(200).json(user);
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400)
+    throw new Error("All fields are mandatory!")
+  }
+  const user = await User.findOne({ email })
+  //compare password with hashedpassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECERT,
+      { expiresIn: "15m" }
+    )
+    res.status(200).json({ accessToken })
+  } else {
+    res.status(401)
+    throw new Error("email or password is not valid")
+  }
 })
 
 //@desc GET the user infnormation
